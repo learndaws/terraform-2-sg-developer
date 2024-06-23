d211ac3dabb0f317da4e47694e45732c5a216220
learndaws <learndaws@gmail.com>
2024-06-23 18:50:39 +0530

    all

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow-specific" {
+resource "aws_security_group" "allow_specific" {
   name        = var.sg_name
   description = var.description
   vpc_id      = var.vpc_id
