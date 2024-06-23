7499e567457429b794f2d96ca2229c2a1efb3fdb
learndaws <learndaws@gmail.com>
2024-06-23 17:54:39 +0530

    all

--- variables.tf
+++ variables.tf
@@ -22,3 +22,29 @@ variable "common_tags" {
 variable "sg_function_tags" {
 
 }
+
+variable "ingress_rules" {
+  default = [
+    {
+        description      = "Allow port 80"
+        from_port        = 80 # 0 means all ports
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+        description      = "Allow port 443"
+        from_port        = 443 # 0 means all ports
+        to_port          = 443
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+        description      = "Allow port 22"
+        from_port        = 22# 0 means all ports
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+}
